686e34b0e68623b020c8682287bbd89eaf0350b5
Austine Gwa <austinegwa64@gmail.com>
2018-06-08 09:46:15 +0300

    js graphics

---
+++ js/roullette.js
@@ -0,0 +1,42 @@
+
+
+window.onload = function(){
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.arc(canvas.width / 2, canvas.height / 2, 70, 0, 2 * Math.PI, false);
+
+context.fillStyle = "#474848";
+context.fill();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.stroke();
+
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI, false);
+
+
+//context.fillStyle = "#13c805";
+//context.fill();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.stroke();
+
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.arc(canvas.width / 2, canvas.height / 2, 130, 0, 2 * Math.PI, false);
+
+
+//context.fillStyle = "#13c805";                //"#f72341";
+//context.fill();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.stroke();
+};
+
+
+
+
+
